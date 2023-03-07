7f60d053f58946b13dfbc4c0f61a549261c562f8
Alex <77668803+Sssanek@users.noreply.github.com>
2023-03-07 13:33:34 +0300

    work on torch > 1.5.0

--- util/misc.py
+++ util/misc.py
@@ -22,9 +22,9 @@ from torch.autograd import Variable
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
 import torchvision
-if float(torchvision.__version__[:3]) < 0.7:
-    from torchvision.ops import _new_empty_tensor
-    from torchvision.ops.misc import _output_size
+# if float(torchvision.__version__[:3]) < 0.7:
+#     from torchvision.ops import _new_empty_tensor
+#     from torchvision.ops.misc import _output_size
 
 
 class SmoothedValue(object):
