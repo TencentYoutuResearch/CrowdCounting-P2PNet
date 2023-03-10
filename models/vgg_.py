b46687cb37492dd77e55e208d7d78b21b7b189a5
eb.sharov <eb.sharov@svo.aero>
2023-03-10 10:07:11 +0300

    fix paths to vgg

--- models/vgg_.py
+++ models/vgg_.py
@@ -25,8 +25,8 @@ model_urls = {
 
 
 model_paths = {
-    'vgg16_bn': './models_storage/vgg16_bn-6c64b313.pth',
-    'vgg16': './models_storage/vgg16-397923af.pth',
+    'vgg16_bn': r'C:\dpm\cv\p2pnet\models_storage\vgg16_bn-6c64b313.pth',
+    'vgg16': r'C:\dpm\cv\p2pnet\models_storage\vgg16-397923af.pth',
 
 }
 
